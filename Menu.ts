e0f1bfab754b75f2ac7d8c21bab11a10c011ec94
caaarolb <carolinelboaventura@gmail.com>
2024-12-11 18:29:46 -0300

    Projeto conta  bancaria

---
+++ Menu.ts
@@ -0,0 +1,133 @@
+import readlinesync = require("readline-sync");
+import { colors } from './src/util/Colors';
+import { Conta } from "./src/model/Conta";
+
+export function main() {
+
+    let opcao: number;
+
+    // Cria novas instâncias (Objetos) da Classe Conta
+    const c1 = new Conta(1, 123, 1, "Anahi", 100000);
+    c1.visualizar();
+
+    // Saque
+    console.log(c1.sacar(200000.00));
+    c1.visualizar();
+
+    const c2 = new Conta(2, 123, 2, "Thalia", 200000);
+    c2.visualizar();
+
+    // depósito
+    c2.depositar(100.00);
+    c2.visualizar();
+
+    while (true) {
+
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", 
+        colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong, 
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nCriar Conta\n\n", colors.reset);
+                
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nApagar uma Conta\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 6:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nSaque\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 7:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nDepósito\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 8:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
+
+                keyPress()
+                break;
+            default:
+                console.log(colors.fg.whitestrong, 
+                    "\nOpção Inválida!\n", colors.reset);
+
+                keyPress()
+                break;
+        }
+    }
+
+}
+
+/* Função com os dados da pessoa desenvolvedora */
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: Jeisa Boaventura ");
+    console.log("https://www.linkedin.com/in/-caroline-boaventura/");
+    console.log("https://github.com/Caaarolb");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
